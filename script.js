6f284c4dc73db374d3d8113fb3be0927053290cf
Pratim <pratim7674ghosh@gmail.com>
2025-03-25 23:03:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const display = document.getElementById("display");
+let isCalculated = false;
+
+function appendToDisplay(input) {
+  if (isCalculated) {
+    display.value = "";
+    isCalculated = false;
+  }
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+  isCalculated = false;
+}
+function calculate() {
+  try {
+    let expression = display.value.replace(/x/g, "*");
+    display.value = eval(expression);
+    isCalculated = true;
+  } catch (error) {
+    display.value = "Error";
+  }
+}
